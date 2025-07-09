a916221f11e0f667b269066c9574d4a4a2a82cc8
giorgi473 <giorgi.kavtaradze2000@mail.ru>
2025-07-09 22:36:24 +0400

    Initial commit

--- src/models/invoice.model.ts
+++ src/models/invoice.model.ts
@@ -1,2 +1,70 @@
 import mongoose from "mongoose";
 
+interface IUserInvoice {
+  name: string;
+  email: string;
+  address1: string;
+  address2?: string;
+  address3?: string;
+}
+
+interface IItem {
+  item_name: string;
+  quantity: number;
+  price: number;
+  total: number;
+}
+
+export interface IInvoice {
+  _id?: mongoose.Types.ObjectId;
+  invoice_no: string;
+  invoice_date: Date;
+  due_date: Date;
+  currency: string;
+  from: IUserInvoice;
+  to: IUserInvoice;
+  items: IItem[];
+  sub_total: number;
+  discount?: number;
+  tax_percentage?: number | null;
+  total: number;
+  notes?: string | null;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+const userInvoiceSchema = new mongoose.Schema<IUserInvoice>(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    address1: { type: String, required: true },
+    address2: { type: String, default: null },
+    address3: { type: String, default: null },
+  },
+  {
+    _id: false,
+  }
+);
+
+const itemSchema = new mongoose.Schema<IItem>(
+  {
+    item_name: { type: String, default: null, required: true },
+    quantity: { type: Number, required: true },
+    price: { type: Number, required: true },
+    total: { type: Number, required: true },
+  },
+  { _id: false }
+);
+
+const InvoiceSchema = new mongoose.Schema<IInvoice>({
+  invoice_no: { type: String, required: true },
+  invoice_date: { type: Date, required: true },
+  due_date: { type: Date, required: true },
+  currency: { type: String, required: true },
+  from: { type: userInvoiceSchema, required: true },
+  to: { type: userInvoiceSchema, required: true },
+  items: { type: [itemSchema], required: true },
+  sub_total: { type: Number, required: true },
+  discount: { type: Number, default: 0 },
+  tax_percentage: {},
+});
